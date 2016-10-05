7a473ff979b37ba6c89775272b57d20bc07749d3
Nicko <nicolasmathieu31@hotmail.com>
2016-10-05 10:59:04 +0200

    Quatre-quart clicker

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-	//votre code
 
+	var counter = 0;
+
+	$(".cake-is-a-lie").click(function(){
+
+		counter++;
+
+		$(".counter").text(counter);
+
+		if(counter % 3 === 0){
+			var tabl = ["c'est bon!!" ,"allez, encore un autre" , "je commence à saturer!" , "bllluuuurrrppp" , "c'est fini pour moi le quatre-quartclicker!!"];
+			var aleat = tabl[Math.floor(Math.random() * tabl.length)];
+			alert(aleat)
+			
+		}
+	});
 
 });
