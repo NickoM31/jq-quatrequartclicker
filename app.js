0d93d1d1d46513a968bac7d0d259da785087bb8b
Amine EL G <aelghayate@simplon.co>
2016-10-07 14:01:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
 	var counter = 0;
-
+	var tabl = [
+		"c'est bon!!" ,"allez, encore un autre" , 
+		"je commence à saturer!" , "bllluuuurrrppp" , 
+		"c'est fini pour moi le quatre-quartclicker!!"
+	];
+	
 	$(".cake-is-a-lie").click(function(){
 
 		counter++;
@@ -9,10 +14,8 @@ $(document).ready(function(){
 		$(".counter").text(counter);
 
 		if(counter % 3 === 0){
-			var tabl = ["c'est bon!!" ,"allez, encore un autre" , "je commence à saturer!" , "bllluuuurrrppp" , "c'est fini pour moi le quatre-quartclicker!!"];
 			var aleat = tabl[Math.floor(Math.random() * tabl.length)];
 			alert(aleat)
-			
 		}
 	});
 
