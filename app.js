dbc831df508e48a7198b1947b4851559df058e2a
Nicko <nicolasmathieu31@hotmail.com>
2016-10-09 14:23:19 +0200

    Quatrequartclicker fini

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
 		$(".counter").text(counter);
 
 		if(counter % 3 === 0){
-			var tabl = ["c'est bon!!" ,"allez, encore un autre" , "je commence à saturer!" , "bllluuuurrrppp" , "c'est fini pour moi le quatre-quartclicker!!"];
+			var tabl = ["c'est bon!!" ,"allez, encore un autre" , "je commence à saturer!" , 
+			"bllluuuurrrppp" , "c'est fini pour moi le quatre-quartclicker!!"];
 			var aleat = tabl[Math.floor(Math.random() * tabl.length)];
 			alert(aleat)
 			
