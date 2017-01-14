9da4d2ad6eef6ef894b500450ff8721edee74ebd
Nicko <nicolasmathieu31@hotmail.com>
2017-01-14 11:44:39 +0100

    Update clean code janvier 2017

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ $(document).ready(function(){
 	var counter = 0;
 
 	$(".cake-is-a-lie").click(function(){
-
 		counter++;
-
 		$(".counter").text(counter);
-
 		if(counter % 3 === 0){
 			var tabl = ["c'est bon!!" ,"allez, encore un autre" , "je commence à saturer!" , 
-			"bllluuuurrrppp" , "c'est fini pour moi le quatre-quartclicker!!"];
+			"arffff ma glycémie..." , "c'est fini pour moi le quatre-quartclicker!!"];
 			var aleat = tabl[Math.floor(Math.random() * tabl.length)];
 			alert(aleat)
-			
 		}
 	});
 
